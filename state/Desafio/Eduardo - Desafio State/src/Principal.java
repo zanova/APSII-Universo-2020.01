2e92aa546da8e33532fc3a8829d9bd719da2d33b
Eduardo junior <39221953+eduardojuniorjf@users.noreply.github.com>
2020-06-06 12:15:56 -0300

    Padrão State

---
+++ state/Desafio/Eduardo - Desafio State/src/Principal.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author Eduardo
+ */
+public class Principal{
+
+    public static void main(String[] args) {
+        Mario mario = new Mario();
+        mario.pegarCogumelo();
+        mario.pegarPena();
+        mario.levarDano();
+        mario.pegarFlor();
+        mario.pegarFlor();
+        mario.levarDano();
+        mario.levarDano();
+        mario.pegarPena();
+        mario.levarDano();
+        mario.levarDano();
+        mario.levarDano();
+    }  
+}
